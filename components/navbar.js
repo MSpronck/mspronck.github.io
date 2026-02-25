ebbf6b5b8768cb14ee3c386e496b41c3f93e82cd
Spronck <m.s.c.spronck@tue.nl>
2026-02-25 10:07:24 +0100

    Moved pages from subdirectory to make links cleaner.

--- components/navbar.js
+++ components/navbar.js
@@ -5,17 +5,17 @@ class Navbar extends HTMLElement {
 
   connectedCallback() {
     let prefix = '';
-    if (document.title != 'Myrthe Spronck') {
-        prefix = '../';
-    }
+    // if (document.title != 'Myrthe Spronck') {
+    //     prefix = '../pages/';
+    // }
     this.innerHTML = `
         <nav class="navbar" id="navbar">
             <a href="javascript:void(0);" class="small-nav-toggle" onclick="toggle_small_nav()">Menu</a>
             <a href="` + prefix + `index.html" class="navbar-first">Home</a>
-            <a href="` + prefix + `pages/publications.html">Publications</a>
-            <a href="` + prefix + `pages/teaching.html">Education</a>
-            <a href="` + prefix + `pages/cv.html">CV</a>
-            <a href="` + prefix + `pages/hobbies.html" class="navbar-last">Non-Academic</a>
+            <a href="` + prefix + `publications.html">Publications</a>
+            <a href="` + prefix + `teaching.html">Education</a>
+            <a href="` + prefix + `cv.html">CV</a>
+            <a href="` + prefix + `hobbies.html" class="navbar-last">Non-Academic</a>
         </nav>
     `;
   }
