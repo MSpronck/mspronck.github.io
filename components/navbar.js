07813e4511c60bcdbcdf8530e1bf77267dd86fdd
Spronck <m.s.c.spronck@tue.nl>
2026-02-25 13:58:00 +0100

    redid file structure for cleaner links

--- components/navbar.js
+++ components/navbar.js
@@ -5,17 +5,17 @@ class Navbar extends HTMLElement {
 
   connectedCallback() {
     let prefix = '';
-    // if (document.title != 'Myrthe Spronck') {
-    //     prefix = '../pages/';
-    // }
+    if (document.title != 'Myrthe Spronck') {
+        prefix = '../';
+    }
     this.innerHTML = `
         <nav class="navbar" id="navbar">
             <a href="javascript:void(0);" class="small-nav-toggle" onclick="toggle_small_nav()">Menu</a>
-            <a href="` + prefix + `index.html" class="navbar-first">Home</a>
-            <a href="` + prefix + `publications.html">Publications</a>
-            <a href="` + prefix + `teaching.html">Education</a>
-            <a href="` + prefix + `cv.html">CV</a>
-            <a href="` + prefix + `hobbies.html" class="navbar-last">Non-Academic</a>
+            <a href="` + prefix + `" class="navbar-first">Home</a>
+            <a href="` + prefix + `publications/">Publications</a>
+            <a href="` + prefix + `teaching/">Teaching</a>
+            <a href="` + prefix + `cv/">CV</a>
+            <a href="` + prefix + `non-academic/" class="navbar-last">Non-Academic</a>
         </nav>
     `;
   }
